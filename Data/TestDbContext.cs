a19db59e2b5849102e366dfa6ba63024a5c0f399
Bumindu Yasith <bumiduyasith@gmail.com>
2021-08-02 12:19:35 +0530

    add refreshtoken model to the dbcontext

--- Data/TestDbContext.cs
+++ Data/TestDbContext.cs
@@ -13,31 +13,10 @@ namespace webapi_identity.DataAccess
     public class TestDbContext : IdentityDbContext
     {
         public DbSet<Item> Items { get; set; }
+        public DbSet<RefreshToken> RefreshToken { get; set; }
 
         public TestDbContext(DbContextOptions<TestDbContext> options) : base(options) { }
 
-        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        // {
 
-
-        //     // Console.WriteLine(_configuration.GetConnectionString("SqlDatabase"));
-        //     //_configuration.GetConnectionString("SqlDatabase"); //
-
-        //     var connectionString = "Server=localhost; Database=CatalogDB; User Id=bumindu; Password=redgreen39";
-
-        //     optionsBuilder.UseSqlServer(connectionString).EnableSensitiveDataLogging();
-
-
-
-        // }
-
-        // protected override void OnModelCreating(ModelBuilder modelBuilder)
-        // {
-        //     base.OnModelCreating(modelBuilder);
-        //     // modelBuilder.Entity<Item>()
-        //     // .Property(p=>p.Price)
-        //     // .HasColumnType("decimal(18,4)");
-
-        // }
     }
 }
