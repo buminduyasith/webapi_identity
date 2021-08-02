e504720b79c993dae73fa9f5979af41d91de3730
Bumindu Yasith <bumiduyasith@gmail.com>
2021-08-02 23:03:32 +0530

    create exceptionhandlingmiddleware

---
+++ Middlewarers/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace webapi_identity.Middlewarers
+{
+    public class ExceptionHandlingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                await context.Response.WriteAsync(ex.Message);
+            }
+        }
+    }
+}
